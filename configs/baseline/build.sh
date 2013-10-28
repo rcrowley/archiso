2d84231f9f62122beabf8bcdd30091e17495b204
Richard Crowley <r@rcrowley.org>
2013-10-28 04:11:19 +0000

    Revert changes to upstream configs/baseline.

--- configs/baseline/build.sh
+++ configs/baseline/build.sh
@@ -10,7 +10,6 @@ arch=$(uname -m)
 work_dir=work
 out_dir=out
 
-pacman_conf=${work_dir}/pacman.conf
 script_path=$(readlink -f ${0%/*})
 
 # Helper function to run make_*() only one time per architecture.
@@ -21,17 +20,9 @@ run_once() {
     fi
 }
 
-# Setup custom pacman.conf with current cache directories.
-make_pacman_conf() {
-    local _cache_dirs
-    _cache_dirs=($(pacman -v 2>&1 | grep '^Cache Dirs:' | sed 's/Cache Dirs:\s*//g'))
-    sed -r "s|^#?\\s*CacheDir.+|CacheDir = $(echo -n ${_cache_dirs[@]})|g" ${script_path}/../releng/pacman.conf > ${pacman_conf}
-}
-
 # Base installation (root-image)
 make_basefs() {
     mkarchiso -v -w "${work_dir}" -D "${install_dir}" init
-    mkarchiso -v -w "${work_dir}" -C "${pacman_conf}" -D "${install_dir}" -p "arch-install-scripts gptfdisk mkinitcpio-nfs-utils rng-tools" install
 }
 
 # Copy mkinitcpio archiso hooks and build initramfs (root-image)
@@ -40,11 +31,6 @@ make_setup_mkinitcpio() {
     cp /usr/lib/initcpio/install/archiso ${work_dir}/root-image/usr/lib/initcpio/install
     cp ${script_path}/mkinitcpio.conf ${work_dir}/root-image/etc/mkinitcpio-archiso.conf
     mkarchiso -v -w "${work_dir}" -D "${install_dir}" -r 'mkinitcpio -c /etc/mkinitcpio-archiso.conf -k /boot/vmlinuz-linux -g /boot/archiso.img' run
-}
-
-# Customize installation (root-image)
-make_customize_root_image() {
-    cp -af ${script_path}/root-image ${work_dir}
 }
 
 # Prepare ${install_dir}/boot/
@@ -90,10 +76,8 @@ make_iso() {
     mkarchiso -v -w "${work_dir}" -D "${install_dir}" -L "${iso_label}" -o "${out_dir}" iso "${iso_name}-${iso_version}-${arch}.iso"
 }
 
-run_once make_pacman_conf
 run_once make_basefs
 run_once make_setup_mkinitcpio
-run_once make_customize_root_image
 run_once make_boot
 run_once make_syslinux
 run_once make_isolinux
